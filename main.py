9681576ee566a815d9f147b07db96bc8ebf42811
IoTIsrael <46277696+IoTIsrael@users.noreply.github.com>
2025-09-04 14:44:33 +0300

    Update main.py for render

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+
+@app.get("/sum")
+async def sum_numbers(a: int, b: int):
+    return {"a": a, "b": b, "sum": a + b}
